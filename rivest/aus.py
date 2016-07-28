8f6188eaf9a25b70775dfe8ecc64a31eabfb8d1a
ron-rivest <rivest@mit.edu>
2016-07-28 13:46:10 -0400

    improved simulated elections in aus.py

--- rivest/aus.py
+++ rivest/aus.py
@@ -3,6 +3,7 @@
 # python3
 # code sketches for AUS senate audit by Bayesian audit method
 
+import collections
 import copy
 import random
 # random.seed(1)    # make deterministic
@@ -34,9 +35,9 @@ class SimulatedElection(Election):
 
     def __init__(self, m, n):
         self.m = m                                 # number of candidates
-        self.candidates = list(range(1,self.m+1))
+        self.candidates = list(range(1,self.m+1))  # {1, 2, ..., m}
         self.n = n                                 # number of cast ballots
-        self.prior_ballots = [ (c,) for c in self.candidates ]
+        self.prior_ballots = [ (c,) for c in self.candidates ] # one for each
 
     def draw_ballots(self, k):
         """ 
@@ -53,9 +54,18 @@ class SimulatedElection(Election):
         return ballots
     
     def scf(self, sample):
-        """ Return result of scf (social choice function) on this sample. """
-        # TBD
-        return tuple(self.candidates)
+        """ 
+        Return result of scf (social choice function) on this sample. 
+
+        Here we use Borda count as a scf.
+        Returns tuple in decreasing order.
+        """
+        counter = collections.Counter()
+        for ballot in sample:
+            for idx, candidate in enumerate(ballot):
+                counter[candidate] += idx
+        return tuple(c for (c, count) in counter.most_common())
+
 
 ##############################################################################
 # A ballot is an abstract blob.
@@ -66,6 +76,7 @@ class SimulatedElection(Election):
 #    -- copying one of them
 #    -- making up a list of "prior ballots" expressing
 #       our Bayesian prior
+#    -- (possibly re-weighting ballots?)
 
 def copy_ballot(b):
     return copy.deepcopy(b)
@@ -76,10 +87,12 @@ def copy_ballot(b):
 
 def urn(election, sample, r):
     """ 
-    Return list of length r generated from sample and prior ballots 
+    Return list of length r generated from sample and prior ballots.
+
     Don't return prior ballots, but sample is part of returned result.
     It may be possible to optimize this code using gamma variates.
     """
+
     L = election.prior_ballots + sample
     for _ in range(r-len(sample)):
         L.append(copy_ballot(random.choice(L)))
@@ -97,43 +110,53 @@ test_urn(SimulatedElection(3,36))
 ##############################################################################
 # Implementation of audit
 
-def audit(election):
-    """ Bayesian audit of given election """
+def audit(election, alpha=0.05, k=4, trials=20):
+    """ 
+    Bayesian audit of given election 
 
+    Input:
+        election     # election to audit
+        alpha        # error tolerance
+        k            # amount to increase sample size by
+        trials       # trials per sample
+    """
     print("audit")
 
     # get basic election info
     candidates = election.candidates
     n = election.n               
 
-    # control parameters
-    alpha = 0.05          # error tolerance
-    k = 4                 # amount to increase sample size by
-    trials = 20           # per sample
-
     # overall audit loop
     sample = []
     while True:
 
-        sample_increment = election.draw_ballots(k)
-        if sample_increment is None:
+        # draw additional ballots and add them to sample
+        sample_increment = election.draw_ballots(k)   
+
+        if sample_increment is []:
             print("Audit has looked at all ballots. Done.")
             break
+
         sample.extend(sample_increment)
-        print("sample is:", sample)
+        print("sample size is", len(sample))
+        # print("sample is:", sample)
 
         # run trials in Bayesian manner
-        outcomes = []
-        for t in range(trials):
-            full_urn = urn(election, sample, n)
-            outcomes.append(election.scf(full_urn))
+        outcomes = [election.scf(urn(election, sample, n))
+                    for t in range(trials)]
+        # we assume that each outcome is 
+        # a list or tuple of candidates who have been elected, 
+        # in some sort of canonical or sorted order.
+        # We can thus test for equality of outcomes.
 
-        # figure out whether to stop or not based on outcomes
-        # fake it for now
-        if random.random()<(float(len(sample))/float(n)):
-            print("Audit confirms outcome; stop.")
+        # find most common outcome and its number of occurrences
+        best, freq = collections.Counter(outcomes).most_common(1)[0]
+        print(best, freq)
+
+        # stop if best occurs almost always
+        if freq >= trials*(1.0-alpha):
+            print("Stopping: audit confirms outcome:", best)
             break
-        # TBD: stop if some outcome happened more than trials*(1-alpha) times
 
 audit(SimulatedElection(4,100))
 
